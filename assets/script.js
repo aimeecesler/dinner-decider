30961512d7cf493042de57601ead78c8b89245a0
AK <icecicle04@gmail.com>
2020-09-17 12:19:31 -0400

    more changes

--- assets/script.js
+++ assets/script.js
@@ -12,9 +12,7 @@ $(document).ready(function () {
 //Variables
 var detailsboxEl = $("#detailsBoxes");
 var randomBtnEL = $("#random-submit");
-var clicked = 0
-
-
+var clicked = 0;
 
 //Functions
 function createBox() {
@@ -47,8 +45,10 @@ function createBox() {
   );
   //text
 
-  //attributes 
-  moreBtn.addClass('button has-text-weight-bold is-primary is-rounded is-normal mt-6 mb-6');
+  //attributes
+  moreBtn.addClass(
+    "button has-text-weight-bold is-primary is-rounded is-normal mt-6 mb-6"
+  );
   //text
 
   h3Name.text("Name");
@@ -79,7 +79,9 @@ function createBox() {
     }).then(function (response) {
       var cuisineArr = response.cuisines;
       for (
-        var cuisineIndex = 0; cuisineIndex < cuisineArr.length; cuisineIndex++
+        var cuisineIndex = 0;
+        cuisineIndex < cuisineArr.length;
+        cuisineIndex++
       ) {
         $(".cuisine-dropdown").append(
           $("<option>").text(cuisineArr[cuisineIndex].cuisine.cuisine_name)
@@ -109,78 +111,52 @@ function createBox() {
     pMenuItem,
     moreBtn
   );
-<<<<<<< HEAD
-<<<<<<< HEAD
-  mapboxgl.accessToken =
-    "pk.eyJ1IjoiaWNlY2ljbGUwNCIsImEiOiJja2Y1aTN0d2QwbjZ2MzJrdXdrb2pkaWh0In0.R70c-4ioETiAZ9SOJgYNlQ";
-  var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11",
-=======
-}
+  function getCuisines() {
+    var queryURL =
+      "https://developers.zomato.com/api/v2.1/cuisines?&lat=" +
+      latitude +
+      "648&lon=" +
+      longitude;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      headers: {
+        "user-key": "1bd06c11f1c9593babc2673ca5dd7d34",
+        "content-type": "application/json",
+      },
+    }).then(function (response) {
+      var cuisineArr = response.cuisines;
+      for (
+        var cuisineIndex = 0;
+        cuisineIndex < cuisineArr.length;
+        cuisineIndex++
+      ) {
+        $(".cuisine-dropdown").append(
+          $("<option>").text(cuisineArr[cuisineIndex].cuisine.cuisine_name)
+        );
+      }
 
-function getCuisines() {
-  var queryURL =
-    "https://developers.zomato.com/api/v2.1/cuisines?&lat=" +
-    latitude +
-    "648&lon=" +
-    longitude;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-    headers: {
-      "user-key": "1bd06c11f1c9593babc2673ca5dd7d34",
-      "content-type": "application/json",
-    },
-  }).then(function (response) {
-    var cuisineArr = response.cuisines;
-    for (
-      var cuisineIndex = 0;
-      cuisineIndex < cuisineArr.length;
-      cuisineIndex++
-    ) {
-      $(".cuisine-dropdown").append(
-        $("<option>").text(cuisineArr[cuisineIndex].cuisine.cuisine_name)
-      );
-    }
->>>>>>> 4e6f7c615073f6b930efed095fd8b1d886a7800f
-  });
-}
-=======
-  moreBtn.on("click", function (event) {
-    event.preventDefault();
-    window.open("details.html")
-  })
-};
->>>>>>> 0c6772ae53c3b7d516aa2d6639e160f28386d28e
+      moreBtn.on("click", function (event) {
+        event.preventDefault();
+        window.open("details.html");
+      });
+    });
 
-// Event Listeners
+    // Event Listeners
 
-randomBtnEL.on("click", function (event) {
-  event.preventDefault();
-  clicked++
-  // console.log(clicked)
-  if (clicked > 3) {
-    detailsboxEl.prepend($("<h2> HANGRY? Pick a place. <h2>").addClass("is-size-1 has-text-weight-bold mt-6"));
-
-  } else {
-    createBox();
+    randomBtnEL.on("click", function (event) {
+      event.preventDefault();
+      clicked++;
+      // console.log(clicked)
+      if (clicked > 3) {
+        detailsboxEl.prepend(
+          $("<h2> HANGRY? Pick a place. <h2>").addClass(
+            "is-size-1 has-text-weight-bold mt-6"
+          )
+        );
+      } else {
+        createBox();
+      }
+    });
   }
-});
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-// <!-- map box map -->
-// <figure class="image is-200x300">
-//   <div
-//     id="map"
-//     style="width: 400px; height: 300px"
-//     class="disabled"
-//   ></div>
-// </figure>
-=======
->>>>>>> 4e6f7c615073f6b930efed095fd8b1d886a7800f
-=======
-
 }
->>>>>>> 0c6772ae53c3b7d516aa2d6639e160f28386d28e
