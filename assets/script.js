8658d78952c81f9c7be802b9f5561615ca0c04d9
AK <icecicle04@gmail.com>
2020-09-17 22:13:51 -0400

    added mapbox to initial user location

--- assets/script.js
+++ assets/script.js
@@ -15,122 +15,148 @@ var clicked = 0;
 var latitude = "";
 var longitude = "";
 
-
-
 //Functions
 
-  //function to create Cusines filter dropdown 
-  function getCuisines() {
-    var queryURL =
-      "https://developers.zomato.com/api/v2.1/cuisines?&lat=" +
-      latitude +
-      "648&lon=" +
-      longitude;
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-      headers: {
-        "user-key": "1bd06c11f1c9593babc2673ca5dd7d34",
-        "content-type": "application/json",
-      },
-    }).then(function (response) {
-      var cuisineArr = response.cuisines;
-      for (
-        var cuisineIndex = 0; cuisineIndex < cuisineArr.length; cuisineIndex++
-      ) {
-        $(".cuisine-dropdown").append(
-          $("<option>").text(cuisineArr[cuisineIndex].cuisine.cuisine_name)
-        );
-      }
-    });
-  }
-
-  window.navigator.geolocation.getCurrentPosition(getCoordinates);
-
-  //Get Coordinates function for users location 
-  function getCoordinates(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-    console.log(latitude, longitude);
-    getCuisines();
-  }
-
-
-  //function to create the details box
-  function createBox() {
-    // console.log("clicked")
-    var randomNum = Math.floor(Math.random() * 19) + 1;
-    // GET the geoloaction for the user
-    //GET cityID for that location
-    // GET cuisines in that location
-    //create a random number between 1-10 to grab a resturant at that index
-    // var ZomatoURL =
-    //  $.ajax(
-    //   url:,
-    //   method: "GET"
-    // )
-    //create elements
-    var detailsBox1 = $("<article>");
-    var h3Name = $("<h3>");
-    var moreBtn = $("<button>");
-    var pAdress = $("<p>");
-    var pNum = $("<p>");
-    var pHours = $("<p>");
-    var pCuisineType = $("<p>");
-    var webURL = $("<a>");
-    var pMenuItem = $("<p>");
-
-    //attributes
-    moreBtn.addClass(
-      "button has-text-weight-bold is-primary is-rounded is-normal mt-6 mb-6"
-    );
-
-    //attributes 
-    moreBtn.addClass('button has-text-weight-bold is-primary is-rounded is-normal mt-6 mb-6');
-    //text
-
-    h3Name.text("Name");
-    pAdress.text("address");
-    pNum.text("Phone Number");
-    pHours.text("Hours");
-    pCuisineType.text("Cuisine");
-    webURL.text("Resturant Link");
-    pMenuItem.text("Top Item");
-    moreBtn.text("More info");
-    
-    //append
-    detailsboxEl.append(detailsBox1);
-    detailsBox1.append(
-      h3Name,
-      pAdress,
-      pNum,
-      pHours,
-      pCuisineType,
-      webURL,
-      pMenuItem,
-      moreBtn
-    );
-    moreBtn.on("click", function (event) {
-      event.preventDefault();
-      window.open("details.html")
-    })
-  };
-
-  // Event Listeners
-
-  randomBtnEL.on("click", function (event) {
-    event.preventDefault();
-    clicked++
-    // console.log(clicked)
-    if (clicked > 3) {
-      detailsboxEl.prepend($("<h2> HANGRY? Pick a place. <h2>").addClass("is-size-1 has-text-weight-bold mt-6"));
-
-    } else {
-      createBox();
+//function to create Cusines filter dropdown
+function getCuisines() {
+  var queryURL =
+    "https://developers.zomato.com/api/v2.1/cuisines?&lat=" +
+    latitude +
+    "648&lon=" +
+    longitude;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    headers: {
+      "user-key": "1bd06c11f1c9593babc2673ca5dd7d34",
+      "content-type": "application/json",
+    },
+  }).then(function (response) {
+    var cuisineArr = response.cuisines;
+    for (
+      var cuisineIndex = 0;
+      cuisineIndex < cuisineArr.length;
+      cuisineIndex++
+    ) {
+      $(".cuisine-dropdown").append(
+        $("<option>").text(cuisineArr[cuisineIndex].cuisine.cuisine_name)
+      );
     }
   });
+}
 
+window.navigator.geolocation.getCurrentPosition(getCoordinates);
 
- 
+//Get Coordinates function for users location
+function getCoordinates(position) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  console.log(longitude, latitude);
+  getCuisines();
+  // var locationCoordinates = [longitude, latitude];
+  // console.log(locationCoordinates);
+}
 
-  
+//function to create the details box
+function createBox() {
+  // console.log("clicked")
+  var randomNum = Math.floor(Math.random() * 19) + 1;
+  // GET the geoloaction for the user
+  //GET cityID for that location
+  // GET cuisines in that location
+  //create a random number between 1-10 to grab a resturant at that index
+  // var ZomatoURL =
+  //  $.ajax(
+  //   url:,
+  //   method: "GET"
+
+  // re pull location coordinates
+  // var locationCoordinates = [latitude, longitude];
+
+  // )// Mapbox token
+  // mapboxgl.accessToken =
+  //   "pk.eyJ1IjoiaWNlY2ljbGUwNCIsImEiOiJja2Y1aTN0d2QwbjZ2MzJrdXdrb2pkaWh0In0.R70c-4ioETiAZ9SOJgYNlQ";
+  // create new map object
+  // var restaurantMap = new mapboxgl.Map({
+  //   container: "detailsBoxes",
+  //   center: [33.8353436, -84.3647096],
+  //   zoom: 14,
+  // });
+  //create elements
+  var detailsBox1 = $("<article>");
+  var h3Name = $("<h3>");
+  var moreBtn = $("<button>");
+  var pAdress = $("<p>");
+  var pNum = $("<p>");
+  var pHours = $("<p>");
+  var pCuisineType = $("<p>");
+  var webURL = $("<a>");
+  var pMenuItem = $("<p>");
+  // // map container div
+
+  // default Map-
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiaWNlY2ljbGUwNCIsImEiOiJja2Y1aTN0d2QwbjZ2MzJrdXdrb2pkaWh0In0.R70c-4ioETiAZ9SOJgYNlQ";
+  var restaurantMap = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+    center: [longitude, latitude], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+  });
+
+  //attributes
+  moreBtn.addClass(
+    "button has-text-weight-bold is-primary is-rounded is-normal mt-6 mb-6"
+  );
+
+  //attributes
+  moreBtn.addClass(
+    "button has-text-weight-bold is-primary is-rounded is-normal mt-6 mb-6"
+  );
+  //text
+
+  h3Name.text("Name");
+  pAdress.text("address");
+  pNum.text("Phone Number");
+  pHours.text("Hours");
+  pCuisineType.text("Cuisine");
+  webURL.text("Resturant Link");
+  pMenuItem.text("Top Item");
+  moreBtn.text("More info");
+
+  //append
+  detailsboxEl.append(detailsBox1);
+  detailsBox1.append(
+    h3Name,
+    pAdress,
+    pNum,
+    pHours,
+    pCuisineType,
+    webURL,
+    pMenuItem,
+    moreBtn,
+    restaurantMap
+  );
+
+  moreBtn.on("click", function (event) {
+    event.preventDefault();
+    window.open("details.html");
+  });
+}
+
+// Event Listeners
+
+randomBtnEL.on("click", function (event) {
+  event.preventDefault();
+  clicked++;
+  // console.log(clicked)
+  if (clicked > 3) {
+    detailsboxEl.prepend(
+      $("<h2> HANGRY? Pick a place. <h2>").addClass(
+        "is-size-1 has-text-weight-bold mt-6"
+      )
+    );
+  } else {
+    createBox();
+  }
+});
