c575b76de56061eb8094116a88152ae0b5f542fb
AK <icecicle04@gmail.com>
2020-09-19 12:21:27 -0400

    added average costs, highlights, timings to details page.

--- assets/detailsScript.js
+++ assets/detailsScript.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
 
   getRestaurantInfo();
   var h1El = $("#rName");
+  var h2El = $("#rTimings");
+  var h3El = $("#rAvgCost");
+  var h4El = $("#rHighlights");
+
   function getRestaurantInfo() {
     var queryURL =
       "https://developers.zomato.com/api/v2.1/restaurant?res_id=" + restID;
@@ -17,7 +21,7 @@ $(document).ready(function () {
       console.log(response);
       var rLatitude = response.location.latitude;
       var rLongitude = response.location.longitude;
-
+      // adds map
       mapboxgl.accessToken =
         "pk.eyJ1IjoiaWNlY2ljbGUwNCIsImEiOiJja2Y1aTN0d2QwbjZ2MzJrdXdrb2pkaWh0In0.R70c-4ioETiAZ9SOJgYNlQ";
       var map = new mapboxgl.Map({
@@ -26,12 +30,20 @@ $(document).ready(function () {
         center: [rLongitude, rLatitude], // starting position [lng, lat]
         zoom: 14, // starting zoom
       });
+      // adds map marker
       var marker = new mapboxgl.Marker()
         .setLngLat([rLongitude, rLatitude])
         .addTo(map);
+      // appending restaurant details
       h1Name = response.name;
-      console.log(h1Name);
+      h2Timings = "Timings: " + response.timings;
+      h3AvgCost = "Average Cost for Two: $ " + response.average_cost_for_two;
+      h4Highlights = "Highlights: " + response.highlights;
+      // console.log(h1Name);
       h1El.append(h1Name);
+      h2El.append(h2Timings);
+      h3El.append(h3AvgCost);
+      h4El.append(h4Highlights);
       // var appendedName = rName;
       // appendedName.append$("rName");
     });
